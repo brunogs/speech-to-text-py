fe9ec8b020cefb4fdb8ec8bdf57cdbf4834034f7
Bruno Silva <bruno.silva@digibee.com>
2024-03-23 19:07:49 -0300

    fix: whisper from openai

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import whisper
 
 def main():
     downloadVideo("https://www.youtube.com/watch?v=0dG7UIWu2ik")
+    loaded_model = load_model()
+    if loaded_model:
+        transcribe_speech(loaded_model, "/resources/good_speech.mp3")
     print("Python application using yt-dlp and whisper")
 
 def downloadVideo(rawUrl):
@@ -14,13 +17,32 @@ def downloadVideo(rawUrl):
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
-        'outtmpl': '/resources/good_speech1'
+        'outtmpl': '/resources/good_speech'
     }
     with yt_dlp.YoutubeDL(yt_opts) as ydl:
         try:
             ydl.download([rawUrl])
         except yt_dlp.DownloadError as e:
             print('Error downloading video:', e)
+
+def load_model():
+    try:
+        model = whisper.load_model('small')
+        print("Model loaded successfully.")
+        return model
+    except Exception as e:
+        print("Error loading model:", e)
+        return None
+
+def transcribe_speech(model, audio_file):
+    try:
+        result = model.transcribe(audio_file)
+        print(result)
+        with open("/resources/transcription.txt", "w") as text_file:
+            text_file.write(result["text"])
+        print("Transcription saved successfully.")
+    except Exception as e:
+        print("Error transcribing speech:", e)
 
 if __name__ == "__main__":
     main()
